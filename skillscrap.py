a65defb7825c1d0f069ef46227084becd74c0b23
tszub <bela.szuber@gmail.com>
2025-09-03 13:59:04 +0200

    get all offer links

--- skillscrap.py
+++ skillscrap.py
@@ -3,14 +3,22 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import json
+import ast
 response = requests.get('https://justjoin.it/job-offers/all-locations/testing')
 soup = BeautifulSoup(response.content, 'html.parser')
 content_div = soup.find('script', type= 'application/ld+json')
 content_div = content_div.text
-content_div = content_div.replace('<script type="application/ld+json"> {"@context":"https://schema.org","@type":"CollectionPage","name":"Job Offers","hasPart": ' , '').replace(']}</script>', ']')
-print(content_div)
+content_div = content_div.replace('<script type="application/ld+json"> {"@context":"https://schema.org","@type":"CollectionPage","name":"Job Offers","hasPart": ' , '').replace(']}', ']')
+content_div = content_div.replace('{"@context":"https://schema.org","@type":"CollectionPage","name":"Job Offers","hasPart":', '').replace('', '')
+content_div = content_div.replace('</script>', '')
 content_div = json.loads(content_div)
+offer_amounts = len(content_div)
 
+i = 0
+while i  < offer_amounts:
+    offer = content_div[i]
+    print(offer['url'])
 
+    i += 1
 
-print(content_div)
+#print(offer_amounts)
