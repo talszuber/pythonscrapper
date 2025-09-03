303e85e1acd98d1197230860e9ebed015476af09
tszub <bela.szuber@gmail.com>
2025-09-03 15:50:31 +0200

    working parser to logs

--- skillscrap.py
+++ skillscrap.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 import json
-import ast
+from csv import writer
 response = requests.get('https://justjoin.it/job-offers/all-locations/testing')
 soup = BeautifulSoup(response.content, 'html.parser')
 content_div = soup.find('script', type= 'application/ld+json')
@@ -15,10 +15,20 @@ content_div = json.loads(content_div)
 offer_amounts = len(content_div)
 
 i = 0
+#with open("offers_skills.csv", "a", newline="",encoding='utf-8-sig') as file:
 while i  < offer_amounts:
+#    writer_object = writer(file)
     offer = content_div[i]
-    print(offer['url'])
-
+    url = offer['url']
+    response = urllib.request.urlopen(url)
+    response_API = requests.get('https://api.justjoin.it/v1/offers/*id*'.replace('*id*', url.split('/')[-1]))
+    response_json = response_API.json()
+    title = response_json['title']
+    company = response_json['companyName']
+    required_skills = response_json['requiredSkills']
+    nth_skills = response_json['niceToHaveSkills']
+    exp = response_json['experienceLevel']['label']
+    print(f' {title}, {exp}, {required_skills}, {nth_skills}')
+#    writer_object.writerow(f'{title}, {company}, {exp}, {required_skills}, {nth_skills}')
     i += 1
-
-#print(offer_amounts)
+print('Done')
